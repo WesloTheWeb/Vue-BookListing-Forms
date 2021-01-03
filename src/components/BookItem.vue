63446b8cc70eaee23e75762e151480a88f0d0863
WesloTheWeb <wesleyadwebster@gmail.com>
2021-01-03 01:02:04 -0600

    initial data passing

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,13 +1,15 @@
 <template>
   <li>
-    {{book.title}} : {{book.author}}
+    {{ book.title }} : {{ book.author }}
+    <span v-if="read">Read</span>
+    <span v-else="read">Not Read</span>
   </li>
 </template>
 
 <script>
 export default {
-  name: 'BookItem',
-  props: ['book'],
+  name: "BookItem",
+  props: ["book"],
 };
 </script>
 
